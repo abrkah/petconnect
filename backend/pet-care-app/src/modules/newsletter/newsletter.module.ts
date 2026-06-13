f4d457f9ffe85e8a4de01901e1a48441120e6805
AB <ab@ABs-MacBook-Pro.local>
2026-06-13 07:10:53 -0700

    Import MailModule in newsletter module#

--- backend/pet-care-app/src/modules/newsletter/newsletter.module.ts
+++ backend/pet-care-app/src/modules/newsletter/newsletter.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MailModule } from '../common/mail.module';
 import { NewsletterSubscriber } from './entities/newsletter-subscriber.entity';
 import { NewsletterController } from './newsletter.controller';
 import { NewsletterService } from './newsletter.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([NewsletterSubscriber])],
+  imports: [TypeOrmModule.forFeature([NewsletterSubscriber]), MailModule],
   controllers: [NewsletterController],
   providers: [NewsletterService],
 })
