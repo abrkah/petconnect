ecf74733fa12b7364db350c6095e90b7880ad980
AB <ab@ABs-MacBook-Pro.local>
2026-06-15 05:44:41 -0700

    chore(message): export ChatGateway and register hire-related entities

--- backend/pet-care-app/src/modules/message/message.module.ts
+++ backend/pet-care-app/src/modules/message/message.module.ts
@@ -7,17 +7,28 @@ import { MessageService } from './message.service';
 import { MessageController } from './message.controller';
 import { Message } from './entities/message.entity';
 import { User } from '../user/entities/user.entity';
+import { OwnerProfile } from '../owner/entities/owner.entity';
+import { ProviderProfile } from '../provider/entities/provider.entity';
+import { HireRequest } from '../hire-requests/entities/hire-request.entity';
+import { ProviderPetAssignment } from '../provider-pet-assignment/entities/provider-pet-assignment.entity';
 import { PresenceService } from './presence.service';
 import { ChatGateway } from './chat.gateway';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Message, User]),
+    TypeOrmModule.forFeature([
+      Message,
+      User,
+      OwnerProfile,
+      ProviderProfile,
+      HireRequest,
+      ProviderPetAssignment,
+    ]),
     JwtModule.registerAsync(jwtConfig.asProvider()),
     ConfigModule.forFeature(jwtConfig),
   ],
   controllers: [MessageController],
   providers: [MessageService, PresenceService, ChatGateway],
-  exports: [MessageService],
+  exports: [MessageService, ChatGateway],
 })
 export class MessageModule {}
